3feecfda195c584560b63a2f3e350f9a37a53bfb
Jeppe O <100990116+OlanderDesign@users.noreply.github.com>
2022-07-25 20:18:40 +0200

    Added TreasureMap code

--- Day4-TreasureMap.py
+++ Day4-TreasureMap.py
@@ -4,15 +4,15 @@ row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
+position = input("Where do you want to put the treasure? ") #23
 # 🚨 Don't change the code above 👆
 
 # Write your code below this row 👇
 
+horizontal = int(position[0]) #2
+vertical = int(position[1]) #3
 
-
-
-
+map[vertical - 1][horizontal - 1] = "X"
 
 # Write your code above this row 👆
 
